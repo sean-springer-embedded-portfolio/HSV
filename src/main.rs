261143356266e67fee35882dd6a4d7da63a6f8f9
Sean Springer <ss28@pdx.edu>
2026-02-21 13:11:43 -0800

    setup

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_main]
+#![no_std]
+
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+use cortex_m_rt::entry;
+use microbit::board::Board;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let _board = Board::take().unwrap();
+
+    loop {}
+}
